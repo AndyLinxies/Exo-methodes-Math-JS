7bfe505b8d1a88d1fa38ba4b1869d634e9d4a9c7
AndyLinxies <linxies@gmail.com>
2021-04-16 11:24:24 +0200

    Methodes Math

---
+++ public/js/main.js
@@ -0,0 +1,27 @@
+let nombre = 8.56;
+//2
+console.log(Math.round(nombre));
+//3
+console.log(Math.ceil(nombre));
+//4
+console.log(Math.floor(nombre));
+//5
+console.log(Math.trunc(nombre));
+//6
+console.log(Math.random().toFixed(1));
+//to Fixed transforme en String
+//7
+console.log(Math.random()*100);
+//On n'aura ni 0 ni 100
+//8
+console.log(Math.round(Math.random()*100));
+//9
+console.log(Math.pow(8,2));
+//10
+console.log(Math.sqrt(9));
+//11
+console.log(Math.abs(-1));
+//12
+console.log(Math.max(-2, 1000, 8, 57));
+//13
+//console.log(Math.min(-2, 1000, 8, 57));
